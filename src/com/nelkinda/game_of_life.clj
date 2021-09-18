95247243e6e5bd807317ba2db6e4ca4490d4603f
Christian Hujer <Christian.Hujer@nelkinda.com>
2021-09-18 21:10:08 +0530

    Minor code improvements.

--- src/com/nelkinda/game_of_life.clj
+++ src/com/nelkinda/game_of_life.clj
@@ -6,22 +6,22 @@
 })
 
 (defn P [x y] {:x x :y y})
+(defn plus [cell1 cell2] (P (+ (:x cell1) (:x cell2)) (+ (:y cell1) (:y cell2))))
 (defn neighbors [cell]
     (def neighbors-of-origin #{(P -1 -1) (P -1 0) (P -1 1) (P 0 -1) (P 0 1) (P 1 -1) (P 1 0) (P 1 1)})
-    (defn plus [cell1 cell2] {:x (+ (:x cell1) (:x cell2)) :y (+ (:y cell1) (:y cell2))})
 
     (map #(plus cell %) neighbors-of-origin))
 
 (defn next-generation [life]
-    (defn is-alive [cell] (contains? life cell))
-    (defn is-dead [cell] (not (is-alive cell)))
-    (defn dead-neighbors [cell] (filter #(is-dead %) (neighbors cell)))
-    (defn live-neighbors [cell] (filter #(is-alive %) (neighbors cell)))
-    (defn count-live-neighbors [cell] (count (live-neighbors cell)))
-    (defn born [cell rules] (contains? (:birth rules) (count-live-neighbors cell)))
-    (defn survives [cell rules] (contains? (:survival rules) (count-live-neighbors cell)))
-    (defn dead-neighbors-of-living-cells [] (mapcat #(dead-neighbors %) life))
-    (defn surviving-cells [] (filter #(survives % rules) life))
-    (defn born-cells [] (filter #(born % rules) (dead-neighbors-of-living-cells)))
+    (defn- is-alive [cell] (contains? life cell))
+    (defn- is-dead [cell] (not (is-alive cell)))
+    (defn- dead-neighbors [cell] (filter #(is-dead %) (neighbors cell)))
+    (defn- live-neighbors [cell] (filter #(is-alive %) (neighbors cell)))
+    (defn- count-live-neighbors [cell] (count (live-neighbors cell)))
+    (defn- born [cell rules] (contains? (:birth rules) (count-live-neighbors cell)))
+    (defn- survives [cell rules] (contains? (:survival rules) (count-live-neighbors cell)))
+    (defn- dead-neighbors-of-living-cells [] (mapcat #(dead-neighbors %) life))
+    (defn- surviving-cells [] (filter #(survives % rules) life))
+    (defn- born-cells [] (filter #(born % rules) (dead-neighbors-of-living-cells)))
 
     (into #{} (concat (surviving-cells) (born-cells))))
